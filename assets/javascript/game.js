39922e92f1e598b8b1e6900434e260871fc034bd
Clark Alms <clark.alms@gmail.com>
2020-03-11 02:02:44 -0500

    all functionality working / the game is on

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,26 +21,62 @@ $(document).ready(function() {
     var word;
     var userGuess ;
     var guesses = [];
-    var tries = 6;
+    var wrongLetters = [];
+    var lives;
     var counter;
     var spaces;
 
     // Elements
-    var myLives = document.getElementById('num-of-tires-remaining');
+    var triesCol = document.getElementById("tries");
+    var numOfTries = document.getElementById('num-of-tries-remaining');
     var displayCategory = document.getElementById('category');
     var letterBtn = document.getElementById('letter-btns');
+    var lettersGuessed = document.getElementById("letters-guessed");
+    var incorrectLettersGuessed = document.getElementById('wrong-letters');
+    var playBtn = document.getElementById('play');
 
-    //Letter Buttons
-    function letterButtons() {
+    // Generate Keyboard
+    buttons = function() {
+        var letters = document.createElement('ul');
         for (var i = 0; i < alphabet.length; i++) {
-            var letterBtn = $("<button>");
-            letterBtn.addClass("letter-button letter letter-button-color");
-            letterBtn.attr("active", alphabet[i]);
-            letterBtn.text(alphabet[i]);
-            $("#letter-btns").append(letterBtn);
-            
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        userClick();
+        letterBtn.appendChild(letters);
+        letters.appendChild(list);
+    }
+  }
+    
+      // OnClick Function
+   userClick = function () {
+        list.onclick = function () {
+        var guess = (this.innerHTML);
+        this.setAttribute("class", "active");
+        this.onclick = null;
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === guess) {
+                guesses[i].innerHTML = guess;
+                counter += 1;
+            } 
+        } 
+
+        var j = (word.indexOf(guess));
+        console.log(guess);
+        if (j === -1) {
+        wrongLetters.push(guess)
+        incorrectLettersGuessed.innerHTML = wrongLetters;  
+        lives -= 1;
+        tries();
+
+        } else {
+            tries();
         }
     }
+  }
+  
+    
 
     // Choose a Category
     function pickCat() {
@@ -63,59 +99,60 @@ $(document).ready(function() {
             correct.setAttribute('id', 'my-word');
             userGuess = document.createElement('span');
             userGuess.setAttribute('class', 'user-guess');
-            console.log();
-            if (word[i] === "-") {
-                console.log("space");
-                // word[i] = word[i].replace("-", ' ');
+            if (word[i] === " ") {
+                userGuess.setAttribute('class', 'user-guess space');
                 userGuess.innerHTML = word[i];
-                spaces = 1;
-                append(userGuess);
+                spaces = 1;    
             } else {
                 userGuess.innerHTML = "_";
             }
             guesses.push(userGuess);
+            
             hiddenWord.appendChild(correct);
             correct.appendChild(userGuess);
         }
     }
 
-    // User Click
-    function userClick() {
-        letterBtn.onclick = function () {
-            var userGuess = (this.innerHTML);
-            this.setAttribute("class", "active");
-            this.onclick = null;
-            for (var i = 0; i < word.length; i++) {
-                if (word[i] === userGuess) {
-                    guesses[i].innerHTML = userGuess;
-                    counter += 1;
-                }
-            }
-            var a = (word.indexOf(userGuess));
-            if (a === -1) {
-                tries -= 1;
-                letterGuess();
-            } else {
-                letterGuess();
-            }
-        }
-    }
 
     // User Lives
     function tries() {
-        myLives.innerHTML = "Tries: " + tries;
-        if (tries < 1) {
-            myLives.innerHTML = "Game Over!"
+        numOfTries.innerHTML = lives;
+        if (lives < 1) {
+            gameOver();
+            // triesCol.setAttribute('class', 'col-6 game-over')
+            // numOfTries.innerHTML = "Game Over!";
         }
         for (i = 0; i < guesses.length; i++) {
             if (counter + spaces === guesses.length) {
-                myLives.innerHTML = "And the Oscar goes to... YOU!";
+                winner();
+                // triesCol.setAttribute('class', 'col-6 winner')
+                // numOfTries.innerHTML = "And the Oscar goes to... YOU!";
             }
         }
     }
 
+
+    // Lose Game Function
+    function gameOver() {
+        lettersGuessed.setAttribute('class', 'col-12');
+        triesCol.setAttribute('class', 'col-12');
+        numOfTries.setAttribute('class', 'game-over')
+        numOfTries.innerHTML = "Game Over!";
+        playBtn.innerHTML = "Play Again";
+    }
+
+
+    // Win Game Function
+    function winner(){
+        lettersGuessed.setAttribute('class', 'col-12');
+        triesCol.setAttribute('class', 'col-12');
+        numOfTries.setAttribute('class', 'winner')
+        numOfTries.innerHTML = "WINNER! WINNER! WINNER!";
+        playBtn.innerHTML = "Play Again";
+    }
     // Play the Game
     function play() {
+        
         wordList = [
             ["Intersteller", "The Dark Knight", "Jaws", "The Lego Movie"],
             ["Rolling In The Deep", "Let It Be", "Smells Like Teen Spirit", "Bohemian Rhapsody"],
@@ -123,23 +160,40 @@ $(document).ready(function() {
             ["George Clooney", "Kim Kardashian", "Dwayne Johnson", "Michael Jordan"]
         ];
         randomCategories = wordList[Math.floor(Math.random() * wordList.length)];
-        word = randomCategories[Math.floor(Math.random() * randomCategories.length)];
-        word = word.replace(/\s/g, "-");
+        word = randomCategories[Math.floor(Math.random() * randomCategories.length)].toLowerCase();
         console.log(word);
-        myLives.innerHTML = "Tries: " + tries;
-        letterButtons();
-        userClick();
+        numOfTries.innerHTML = tries;
         guesses = [ ];
-        tries = 6;
+        wrongLetters = [ ];
+        lives = 6;
         counter = 0;
         spaces = 0;
+        buttons();
+        tries();
         letterGuess()
         pickCat();
     }
-
-
     play();
+    // playBtn.onclick = function(){
+    // play();
+    // playBtn.innerHTML = "Play Again";
+    // document.getElementById("play").id = "reset";
+    // }
 
-
+    playBtn.onclick = function() {
+    var correct = document.getElementById('my-word');
+    var letters = document.getElementById('alphabet');
+    lettersGuessed.setAttribute('class', 'col-6');
+    triesCol.setAttribute('class', 'col-6');
+    numOfTries.id = "num-of-tries-remaining";
+    numOfTries.className = 'number';
+    playBtn.innerHTML = "Play";
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    incorrectLettersGuessed.innerHTML = "";
+    randomCategories;
+    word;
+    play();
+    }
 
 })
